db8bcced436f0e455a061e1c366546c067f9dce0
Santiago Sotelo <santiago.sotelo@konecta.com.py>
2026-01-30 12:56:31 -0300

    refactor: detalle cursos

--- src/app/courses/pages/detalle-leccion/detalle-leccion.component.ts
+++ src/app/courses/pages/detalle-leccion/detalle-leccion.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { response } from '../../../interfaces/response.model';
 import { Leccion } from '../../../interfaces/leccion.interface';
@@ -6,43 +6,67 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { CoursesService } from '../../CoursesServices/courses.service';
 import { MatIcon } from "@angular/material/icon";
 import { MatButton } from "@angular/material/button";
-import { MatCardContent, MatCard, MatCardActions, MatCardHeader, MatCardTitle, MatCardSubtitle } from "@angular/material/card";
+import { MatCardContent, MatCard, MatCardActions, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardAvatar } from "@angular/material/card";
+import { MatGridList, MatGridTile } from "@angular/material/grid-list";
+import { MatActionList } from "@angular/material/list";
+import { CdkTableModule } from "@angular/cdk/table";
 
 @Component({
   selector: 'app-detalle-leccion',
   standalone: true,
-  imports: [MatIcon, MatButton, MatCardContent, MatCard, MatCardActions, MatCardHeader, MatCardTitle, MatCardSubtitle],
+  imports: [
+    MatIcon,
+    MatButton,
+    MatCardContent,
+    MatCard,
+    MatCardActions,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatGridList,
+    MatGridTile,
+    MatActionList,
+    MatCardAvatar,
+    CdkTableModule
+],
   templateUrl: './detalle-leccion.component.html',
-  styleUrl: './detalle-leccion.component.css'
+  styleUrl: './detalle-leccion.component.css',
 })
 export class DetalleLeccionComponent implements OnInit {
-
   route = inject(ActivatedRoute);
   cursosService = inject(CoursesService);
   sanitizer = inject(DomSanitizer);
   router = inject(Router);
+
+  isMobile = window.innerWidth <= 768;
 
   idCurso!: string;
   idLeccion!: string;
   leccion!: Leccion;
   videoUrl!: SafeResourceUrl;
 
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth <= 768;
+  }
+
   ngOnInit(): void {
     this.idCurso = this.route.snapshot.params['idCurso'];
     this.idLeccion = this.route.snapshot.params['idLeccion'];
 
-    this.cursosService.listar(`/api/lessons/${this.idLeccion}`).subscribe(
-      (response: response) => {
+    this.cursosService
+      .listar(`/api/lessons/${this.idLeccion}`)
+      .subscribe((response: response) => {
         if (response.status == 'SUCCESS') {
           this.leccion = response.data;
-          this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.leccion.youtubeUrl);
+          this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+            this.leccion.youtubeUrl,
+          );
         }
-      }
-    );
+      });
   }
 
-  goBack(){
+  goBack() {
     this.router.navigate([`/cursos/${this.idCurso}`]);
   }
-
 }
